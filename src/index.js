f3fe60fbedfd3a655ef6f6fff7f0a1e429e964f6
diegomac <dijavaji@gmail.com>
2025-12-26 17:06:24 -0500

    agrego servidor con express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import 'dotenv/config';
-import { conectarWhatsApp } from './WhatsAppClient.js';
+import express from 'express';
+//import { conectarWhatsApp } from './WhatsAppClient.js';
+//console.log('\uf232 Iniciando WhatsApp Bot...');
+//conectarWhatsApp().catch(console.error);
 
-console.log('\udb85\udcde Iniciando WhatsApp Bot...');
-conectarWhatsApp().catch(console.error);
+const PORT = process.env.PORT ?? 1234;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo desde Express')
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor esta corriendo en http://localhost:${PORT} \uf427`)
+})
 
